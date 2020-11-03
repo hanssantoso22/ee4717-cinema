1fdd29dfcccc1d93ac5abd873af9c501f3419010
DarylTay <48782653+DarylTay@users.noreply.github.com>
2020-11-03 15:41:24 +0800

    Will no longer show expired sessions.
    Added description for cinemas

--- cinemas/cinema_page.php
+++ cinemas/cinema_page.php
@@ -14,20 +14,35 @@
                <div class="col-2"><a class="tab" href="../index.php">MOVIES</a></div>
                <div class="col-2"><a class="tab active" href="cinemas.php">CINEMAS</a></div>
                <div class="col-2"><a class="tab" href="../bookings/bookings.php">BOOKINGS</a></div>
-               <div class="col-2"></div>
-               <div class="col-2"><a class="cart" href="../booking_cart/booking_cart.php">shopping_cart</a></div>
+			   <?php
+                    session_start();
+                    if(isset( $_SESSION['SESS_MEMBER_ID']) && !empty($_SESSION['SESS_MEMBER_ID']))
+                    {	
+                        echo'<div class="col-3 login-container"><a href="../login/logout.php"><span class="username">'.$_SESSION["fname"].'</span><span class="logout">exit_to_app</span></a></div>
+                            '
+                        ;
+                    }
+                    else
+                    {	
+                        echo'<div class="col-3 login-container"><a class="login" href="../login/login.php">account_circle</a></div>
+                            '
+                        ;
+                    }
+                ?>
+                <div class="col-1 cart-container"><a class="cart" href="./booking_cart/booking_cart.php">shopping_cart</a></div>
            </div>
        </div>
        <div id="main-body">
             <div id="content-box">
+				<a href="cinemas.php">< Back to Cinemas</a><br><br>
 				<?php
-                    session_start();
                     include '../config.php';
 					include '../constants.php';
                     $cinema_id = $_GET['cinema_id'];
 					$query = "SELECT * FROM `cinemas` WHERE `id`=$cinema_id";
                     $cinema = $db->query($query);
                     $row = $cinema->fetch_assoc();
+					$qty =1;
 					ini_set('display_errors', 1);
 					ini_set('display_startup_errors', 1);
 					error_reporting(E_ALL);
@@ -39,60 +54,70 @@
 					<div class="col-3">
 						<div id="cin-pic">
 							<?php
-                            echo '<img src="..'.$row['picture'].'" style="width:auto;" width="400" height="400">';
+                            echo '<img src="..'.$row['picture'].'" style="width:100%; margin-top:20px;" >';
 							?>
 						</div>
 					</div>
-					<div id="cin-txt">
-						<?php echo '<p>'.$row['description'].'</p>' ;?>
+					<div class="col-8" style="padding-left:40px">
+						<p class="grey-5" >Information: </p>
+						<p class="grey-7 description"><?php echo $row['description'];?></p>
+					</div>
+					<div class="col-12" style="padding-left: 20px;">
+						<p class="grey-5" >Buy ticket(s) </p>
+						<form action="../movies/movie_seat_selection.php" method="GET">
+						<?php
+							echo '<p class="grey-5" style="display: inline;">Qty: </p><input type="number" min="1" max="10" name="qty" value="'.$qty.'">&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="Select Seats">'
+						?>
+						<p class="grey-5">Choose schedule: </p>
+						<table>
+							<?php
+							$day=array($today, date('Y-m-d',strtotime("+1 day", strtotime($today))), date('Y-m-d',strtotime("+2 day", strtotime($today))), date('Y-m-d',strtotime("+3 day", strtotime($today))), date('Y-m-d',strtotime("+4 day", strtotime($today))), date('Y-m-d',strtotime("+5 day", strtotime($today))), date('Y-m-d',strtotime("+6 day", strtotime($today))));
+							?>
+							<tr>
+								<th><p>Movies</p></th>
+								<?php
+								for ($x=0;$x<7;$x++){
+									echo'<th>'.date('d M',strtotime($day[$x])).'</th>';										
+								}
+								?>
+							</tr>
+							<?php
+							$query = "SELECT id, movie_id, CAST(timing AS DATE) date, timing AS time_movie FROM movsessions WHERE `cinema_id`=$cinema_id ORDER BY `movie_id` ASC, `timing` ASC";
+							$movie_sess = $db->query($query);					
+							$query2= "SELECT id, movie_name FROM movies ORDER BY 'id' ASC";
+							$movie_name = $db->query($query2);
+							$movie_row=0;
+							$row = $movie_sess->fetch_assoc();
+							while ($row2 = $movie_name->fetch_assoc()){
+								$movie_row=$row2['id'];
+								while (!in_array($row['date'],$day )){
+									$row = $movie_sess->fetch_assoc();	
+								}
+								if ($movie_row==$row['movie_id'] && in_array($row['date'],$day)){
+									echo '<tr>
+											<td>'.$row2['movie_name'].'</td>';
+									for ($x=0;$x<7;$x++){
+										echo'<td>';										
+										while ($day[$x]==$row['date']){	
+											if(date('G:i')<date('G:i',strtotime($row['time_movie']))){
+												echo '<input type="radio" name="movie_session_id" value="'.$row['id'].'">'.date('G:i',strtotime($row['time_movie'])).'</input><br>';
+											}
+											global $row;
+											$row = $movie_sess->fetch_assoc();							
+										}									
+										echo'</td>';
+									}
+									echo '</tr>';
+									while (!in_array($row['date'],$day)){
+										if (!$row = $movie_sess->fetch_assoc())
+											break;
+									}
+								}
+							}
+							?>
+						</table>
 					</div>
 				</div>
-				<div class="row">
-				</div>
-            </div>
-			<div class="row">
-				<table>
-					<?php
-					$day=array($today, date('Y-m-d',strtotime("+1 day", strtotime($today))), date('Y-m-d',strtotime("+2 day", strtotime($today))), date('Y-m-d',strtotime("+3 day", strtotime($today))), date('Y-m-d',strtotime("+4 day", strtotime($today))), date('Y-m-d',strtotime("+5 day", strtotime($today))), date('Y-m-d',strtotime("+6 day", strtotime($today))));
-					?>
-					<tr>
-						<th><p>Movies</p></th>
-						<?php
-						for ($x=0;$x<7;$x++){
-							echo'<th>'.date('d M',strtotime($day[$x])).'</th>';										
-						}
-						?>
-					</tr>
-					<?php
-					$query = "SELECT id, movie_id, CAST(timing AS DATE) date, timing AS time_movie FROM movsessions WHERE `cinema_id`=$cinema_id ORDER BY `movie_id` ASC, `timing` ASC";
-					$movie_sess = $db->query($query);					
-					$query2= "SELECT id, movie_name FROM movies ORDER BY 'id' ASC";
-					$movie_name = $db->query($query2);
-					$movie_row=0;
-					$row = $movie_sess->fetch_assoc();
-					while ($row2 = $movie_name->fetch_assoc()){
-						$movie_row=$row2['id'];
-						if ($movie_row==$row['movie_id']){
-							echo '<tr>
-								  <td>'.$row2['movie_name'].'</td>';
-							for ($x=0;$x<7;$x++){
-								echo'<td>';										
-								while ($day[$x]==$row['date']){	
-									echo '<a href="../movies/seat_selection.php?movie_session_id='.$row['id'].'">'.date('G:i',strtotime($row['time_movie'])).'</a><br>';
-									global $row;
-									$row = $movie_sess->fetch_assoc();							
-								}									
-								echo'</td>';
-							}
-							echo '</tr>';
-							while (!in_array($row['date'],$day)){
-								if (!$row = $movie_sess->fetch_assoc())
-									break;
-							}
-						}
-					}
-					?>
-				</table>
 			</div>
        </div>
        <div id="main-footer">
